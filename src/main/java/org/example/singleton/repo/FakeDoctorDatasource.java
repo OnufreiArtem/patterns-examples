767deb2f827c8f4dc57c4c17a910aa16a250dde7
Artem Onufrei <2hilderoy2@gmail.com>
2021-09-09 19:02:38 +0300

    Created singleton doctor datasource

---
+++ src/main/java/org/example/singleton/repo/FakeDoctorDatasource.java
@@ -0,0 +1,71 @@
+package org.example.singleton.repo;
+
+import org.example.singleton.model.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FakeDoctorDatasource implements CRUDRepo<Doctor> {
+
+	private List<Doctor> doctorDatabase = new ArrayList<>();
+	private static long id = 0L;
+
+	private static FakeDoctorDatasource instance = null;
+
+	private FakeDoctorDatasource() {
+		initFakeData();
+	}
+
+	public static FakeDoctorDatasource getInstance() {
+		if (instance == null) {
+			instance = new FakeDoctorDatasource();
+		}
+
+		return instance;
+	}
+
+	private void initFakeData() {
+		this.addMany(
+				new Doctor(id++, "Scarlett Johansson", "PH"),
+				new Doctor(id++, "Elizabeth Olsen", "PH"),
+				new Doctor(id++, "Robert Downey Jr", "PH"),
+				new Doctor(id++, "Benedict Cumberbatch", "PH"),
+				new Doctor(id++, "Tom Holland", "PH")
+		);
+	}
+
+	@Override
+	public void add(Doctor item) {
+		item.setId(id++);
+		doctorDatabase.add(item);
+	}
+
+	@Override
+	public void addMany(Doctor... list) {
+		for (Doctor doctor : list) {
+			add(doctor);
+		}
+	}
+
+	@Override
+	public void update(Doctor item) {
+		doctorDatabase = doctorDatabase.stream().map(it -> Objects.equals(it.getId(), item.getId()) ? item : it).collect(Collectors.toList());
+	}
+
+	@Override
+	public void remove(Long id) {
+		doctorDatabase = doctorDatabase.stream().filter(it -> !Objects.equals(it.getId(), id)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Doctor get(Long id) {
+		return doctorDatabase.stream().filter(it -> Objects.equals(it.getId(), id)).findFirst().orElse(null);
+	}
+
+	@Override
+	public List<Doctor> getAll() {
+		return doctorDatabase;
+	}
+}
